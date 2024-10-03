fae70e93b696f4f796ff6404222f5d652da3b7d8
Дмитрий Калашников <163724719+BebriJosti@users.noreply.github.com>
2024-10-03 22:55:14 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,7 @@ function createCard(name, link, removeCard) {
 
   cardElement.querySelector(".card__title").textContent = name;
   cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector('.card__image').alt = name;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", removeCard);
