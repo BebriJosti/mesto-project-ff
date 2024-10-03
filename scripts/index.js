1518a6d60963a7964963e38bf3b3ef23ab90c001
Дмитрий Калашников <163724719+BebriJosti@users.noreply.github.com>
2024-10-03 21:20:21 +0500

    prettier

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,27 @@
 
 // @todo: Вывести карточки на страницу
 
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardTemplate = document.querySelector('#card-template').content;
-
-const cardsContainer = document.querySelector('.places__list');
+const cardsContainer = document.querySelector(".places__list");
 
 function createCard(name, link, removeCard) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-  cardElement.querySelector('.card__title').textContent = name;
-  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
 
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', removeCard);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", removeCard);
   return cardElement;
 }
 
-function removeCard(evt) {  
-  const card = evt.target.closest('.card');
+function removeCard(evt) {
+  const card = evt.target.closest(".card");
   card.remove();
 }
 
 for (const element of initialCards) {
-    const card = createCard(element.name, element.link, removeCard);
-    cardsContainer.append(card);
-  }
+  const card = createCard(element.name, element.link, removeCard);
+  cardsContainer.append(card);
+}
