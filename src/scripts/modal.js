52802d53029975e41734e97f8990dfa088d1fc34
Дмитрий Калашников <163724719+BebriJosti@users.noreply.github.com>
2024-11-17 23:00:10 +0500

    fix
    убрал функции обёртки и починил удаление слушателя

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,10 +1,32 @@
+let handlerEscKeyPress
+function onEscKeyPress  (popup) {
+    return function (event) {
+        if (event.key === 'Escape') {
+            closeModal(popup);
+        }
+    };
+}
+
+let handlerOutsideClick
+function outsideKeyPress  (popup) {
+    return function (event) {
+        if (event.target === popup) {
+            closeModal(popup);
+         }
+    };
+}
+
 export function openModal(popup) {
     if (popup) {
-        handleOutsideClick(popup)
-        handleEscKeyPress(popup)
+        onEscKeyPress(popup)
         popup.classList.add('popup_is-opened');
         popup.classList.add('popup_is-animated');
 
+        handlerEscKeyPress = onEscKeyPress(popup);
+        document.addEventListener('keydown', handlerEscKeyPress);
+
+        handlerOutsideClick = outsideKeyPress(popup);
+        document.addEventListener('click', handlerOutsideClick);
     } else {
         console.error(`Popup "${popup}" не найден.`);
     }
@@ -15,38 +37,12 @@ export function closeModal(popup) {
         popup.classList.add('popup_is-animated');
         popup.classList.remove('popup_is-opened');
 
+        document.removeEventListener('keydown', handlerEscKeyPress);
+        document.removeEventListener('click', handlerOutsideClick);
+
         setTimeout(() => popup.classList.remove('popup_is-animated'), 500)
+
     } else {
         console.error(`Popup с селектором "${popup}" не найден.`);
     }
 }
-
-function handleOutsideClick(popup) {
-    const outsideClickHandler = (event) => {
-        if (event.target === popup) {
-            closeModal(popup);
-        }
-    };
-
-    document.addEventListener('click', outsideClickHandler);
-
-    return () => {
-        document.removeEventListener('click', outsideClickHandler);
-    };
-}
-
-
-function handleEscKeyPress(popup) {
-    const escKeyPressHandler = (event) => {
-        if (event.key === 'Escape') {
-            closeModal(popup);
-        }
-    };
-
-    document.addEventListener('keydown', escKeyPressHandler);
-
-    return () => {
-        document.removeEventListener('keydown', escKeyPressHandler);
-    };
-}
-
